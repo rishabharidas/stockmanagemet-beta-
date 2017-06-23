01cd584ccb445fefc00660027fff59034b03b386
Rishab Haridas <rishabharidas@users.noreply.github.com>
2017-06-23 20:58:13 +0530

    Add files via upload

---
+++ Comp_inventory/src/management/Row.java
@@ -0,0 +1,25 @@
+package management;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author acer
+ */
+public class Row {
+    private static int row;
+    public static int getRow(){
+        return row;
+    }
+    public static void setRow(){
+        row++;
+    }
+    public static void setRow( int temp){
+        row= temp;
+    }
+
+
+}
